01cd80e29680f62e1f8207e291b96e1fec2d7ed8
Lucas Beraldi <lucas.beraldiso@gmail.com>
2023-08-15 17:28:04 -0400

    Atualizações

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  const cbx = document.getElementById('cbx');
-  cbx.addEventListener('change', function() {
-    document.body.classList.toggle('dark')
-  });
-});
